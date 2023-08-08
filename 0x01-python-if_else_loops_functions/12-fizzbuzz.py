128ec08aa106894d6345300a656859f0fd23998c
Charles DZADU <dkccharlie@gmail.com>
2023-08-08 23:17:10 +0000

    Fix: Fix pycode style

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 for a in range(1, 101):
-	if a % 3 == 0 and a % 5 == 0:
-		print("FizzBuzz", end=" ")
-	elif a % 3 == 0:
-		print("Fizz", end=" ")
-	elif a % 5 == 0:
-		print("Buzz", end=" ")
-	else:
-		print(a, end=" ")
+    if a % 3 == 0 and a % 5 == 0:
+        print("FizzBuzz", end=" ")
+    elif a % 3 == 0:
+        print("Fizz", end=" ")
+    elif a % 5 == 0:
+        print("Buzz", end=" ")
+    else:
+        print(a, end=" ")
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for a in range(0, 9):
-	for b in range(a + 1, 10):
-		if a != 8:
-			print("{}{}".format(a, b), end=", ")
-		else:
-			print("{}{}".format(a, b))
+    for b in range(a + 1, 10):
+        if a != 8:
+            print("{}{}".format(a, b), end=", ")
+        else:
+            print("{}{}".format(a, b))
