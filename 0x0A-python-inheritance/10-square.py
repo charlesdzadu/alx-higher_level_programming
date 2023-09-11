966737f634727a2c477ac8159561ea2c2420adeb
Charles DZADU <dkccharlie@gmail.com>
2023-09-11 22:29:14 +0000

    :fire: Finish mandatory task

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module to create a class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class that inherits from Rectangle"""
+
+    def __init__(self, size):
+        """Instantiation with size"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
