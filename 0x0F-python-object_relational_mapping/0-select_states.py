eeadb30e3f4beb1b262e2939461b5f58eca95a23
Charles DZADU <dkccharlie@gmail.com>
2023-11-20 05:50:07 +0000

    feat: Correct first task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-
-
+"""  lists all states from the database """
 import MySQLdb
-from sys import argv
+import sys
 
-'''
-Lists all states
-from the database
-'''
+
 if __name__ == "__main__":
-    con = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1],
-        password=argv[2], database=argv[3])
-    curs = con.cursor()
-    curs.execute("SELECT * FROM states ORDER BY id ASC")
-    db = curs.fetchall()
-    for i in db:
-        print(i)
-    curs.close()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
     db.close()
